a8fb91438d871adf48c02542a9380ed3b77d6f27
drgomesp <drgomesp@gmail.com>
2022-08-18 14:25:09 -0300

    feat: Fixing runtime compatibility issues (v2)

--- src/handlers/zapierTrigger.ts
+++ src/handlers/zapierTrigger.ts
@@ -2,15 +2,11 @@ import { fetchStatuses } from './validators';
 import ky from "ky";
 
 export function zapierTrigger(event: Event) {
-    (async () => {
-        const statuses = fetchStatuses();
+    const statuses = fetchStatuses();
 
-        try {
-            const res = await ky.post(ZAPIER_WEBHOOK_URL, { json: statuses }).json;
-
-            console.log('Res: ', res);
-        } catch (err) {
-            console.error(err);
-        }
-    })()
+    ky.post(ZAPIER_WEBHOOK_URL, { json: statuses }).json().then(res => {
+        console.log('Res: ', res);
+    }).catch(err => {
+        console.error(err);
+    });
 }
