1116d57fc31aafadf72ff80840cb85a418363290
drgomesp <drgomesp@gmail.com>
2022-08-24 09:37:24 -0300

    feat(validator-status-cloudflarekv): Implemented missing endponts ande degraded (<80% active blocks)

--- src/handlers/validators/degraded.ts
+++ src/handlers/validators/degraded.ts
@@ -1,7 +1,16 @@
 import { Request } from "itty-router";
+import { fetchStatusesByState } from "../validators";
+import { ValidatorStatus } from "../../types/types";
 
-export async function handlerTombstoned(request: Request): Promise<Response> {
-    return new Response(JSON.stringify([]), {
+export async function handlerDegraded(request: Request): Promise<Response> {
+    let filtered = new Array<ValidatorStatus>();
+    for (const a of await fetchStatusesByState("active")) {
+        if (a.activeBlocks < 80) {
+            filtered.push(a)
+        }
+    }
+
+    return new Response(JSON.stringify(filtered), {
         headers: {
             'content-type': 'application/json;charset=UTF-8',
         },
--- src/handlers/validators/neverJailed.ts
+++ src/handlers/validators/neverJailed.ts
@@ -1,6 +1,6 @@
 import { Request } from "itty-router";
 
-export async function handlerTombstoned(request: Request): Promise<Response> {
+export async function handlerNeverJailed(request: Request): Promise<Response> {
     return new Response(JSON.stringify([]), {
         headers: {
             'content-type': 'application/json;charset=UTF-8',
