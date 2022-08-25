ea6d98294a3e3faa30556335d0cab2e1874b4b71
drgomesp <drgomesp@gmail.com>
2022-08-25 20:21:58 -0300

    feat(validator-status-cache): Fixed bindings

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -1,11 +1,3 @@
-import { webhookTriggers } from "./handlers/webhookTriggers";
-
-export default {
-    async scheduled(event: any, env: any, ctx: any) {
-        ctx.waitUntil(webhookTriggers(event, env));
-    },
-}
-
 declare global {
     const GRAPHQL_API: string;
     const WEBHOOK_URL: string;
